295e0c52875263953bcf267b1a8f36d5a6ed8a64
Blythe McFall <mcfall.blythe@gmail.com>
2018-05-10 01:05:09 -0600

    Iteration 3 - Bubble Sort

--- lib/deck.rb
+++ lib/deck.rb
@@ -10,6 +10,35 @@ class Deck
     @cards.count
   end
 
-  def sort
+=begin
+  #bubble sort method - not functional
+  def sort_cards(deck)
+    sort_three = deck.length
+
+    loop do
+      #[card_3.value, card_2.value, card_1.value]
+      #[card_2.value, card_3.value, card_1.value]
+      #[card_2.value, card_1.value, card_3.value]
+      #[card_1.value, card_2.value, card_3.value]
+
+      #Checks to see if an item is false; conditional used to break out of loop.
+      swapped = false
+      index = cards.@value
+      #Total number of elements in array - 1.
+      (sort_three - 1).times do |index|
+
+        #Accessing an element in the array with iterator, i is the .
+        #If the sort_three.value of
+        if sort_three[index] > sort_three[index + 1]
+          sort_three[index], array[index + 1] = sort_three[index + 1], sort_three[index]
+          swapped = true
+        end
+      end
+      break if not swapped
+
+    end
+    sort_three
   end
+=end
+
 end
