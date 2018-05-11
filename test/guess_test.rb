d4ec9b127516ff46c603873c9887abd4355154b7
Blythe McFall <mcfall.blythe@gmail.com>
2018-05-11 17:36:50 -0600

    Refactored guess_test.rb

--- test/guess_test.rb
+++ test/guess_test.rb
@@ -4,39 +4,38 @@ require './lib/guess'
 require './lib/card'
 
 class GuessTest < Minitest::Test
+
+  def setup
+    card = Card.new("10", "Hearts")
+    @guess = Guess.new("10 of Hearts", card)
+  end
+
   def test_guess_exists
-    card = Card.new("10", "Hearts")
-    guess = Guess.new("10 of Hearts", card)
-
-    assert_instance_of Guess, guess
+    assert_instance_of Guess, @guess
   end
 
-  def test_guess_has_a_card
-    card = Card.new("10", "Hearts")
-    guess = Guess.new("10 of Hearts", card)
-
-    assert_instance_of Card, guess.card
+  def test_guess_created_with_card
+    assert_instance_of Card, @guess.card
   end
 
+  #Two Thumbs Up!  Excellent!
   def test_guess_has_a_response
-    card = Card.new("10", "Hearts")
-    guess = Guess.new("10 of Hearts", card)
-
-    assert_equal "10 of Hearts", guess.response
+    assert_equal "10 of Hearts", @guess.response
   end
 
   def test_guess_is_true
-    card = Card.new("10", "Hearts")
-    guess = Guess.new("10 of Hearts", card)
+    assert_equal true, @guess.correct?
+  end
 
-    assert_equal true, guess.correct?
+  def test_guess_is_false
+    card = Card.new("10", "Hearts")
+    guess = Guess.new("Jack of Spades", card)
+
+    refute guess.correct?
   end
 
   def test_guess_feedback_correct
-    card = Card.new("10", "Hearts")
-    guess = Guess.new("10 of Hearts", card)
-
-    assert_equal "Correct!", guess.feedback
+    assert_equal "Correct!", @guess.feedback
   end
 
   def test_guess_feedback_incorrect
